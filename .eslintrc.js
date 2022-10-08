f9f218e582a57d4bc818f8bf928b2a0ec20c3bf8
yeyan1996 <1996yeyan@gmail.com>
2022-10-08 12:20:39 +0800

    refactor: use @antfu eslint config & update pkg version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,3 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    node: true
-  },
-  extends: ["airbnb-base", "plugin:prettier/recommended"],
-  rules: {
-    "no-param-reassign": "off",
-    "global-require": "off",
-    "import/no-dynamic-require": "off",
-    "import/no-unresolved": "off",
-    "no-console": "warn",
-    "no-debugger": "warn"
-  }
-};
+  extends: '@antfu',
+}
